b339354ade2b2fe8f062f3278baffd192bc5033f
rcoletti116 <coletti.robert@gmail.com>
2025-09-27 16:33:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const Constants                     = require('./constants');
 const DeviceMapper                  = require('./lib/device-mapper');
 const { HomeyAPI }                  = require('./modules/homey-api');
 const {
-  Bridge, Service, Characteristic,
-  Accessory, AccessoryEventTypes, uuid } = require('./modules/hap-nodejs');
+  Bridge, Service, Characteristic, Categories,Accessory,AdaptiveLightingController, AccessoryEventTypes, uuid } = require('hap-nodejs');
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -42,6 +41,13 @@ module.exports = class HomeKitty extends Homey.App {
 
     // create persistence directory
     await this.initializePersistence();
+
+// *** HAP ***
+const hapStoragePath = pathJoin(this.#persistDir, 'hap-storage');
+require('hap-nodejs').HAPStorage.setCustomStoragePath(hapStoragePath);
+this.log(`HAP storage path set to ${ hapStoragePath }`);
+// *** End block ***
+
 
     // perform a reset
     if (Homey.env.HOMEKITTY_RESET === 'true') {
@@ -251,7 +257,7 @@ module.exports = class HomeKitty extends Homey.App {
         port,
         setupID,
         pincode,
-        category: Accessory.Categories.BRIDGE
+        category: Categories.BRIDGE
       });
       this.log('  - started successfully! 🥳');
       // store current credentials
